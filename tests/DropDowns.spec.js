7ddb2a7cca5c5528793cbd8ddbad882db91278b6
suleman <sulemmanbhatti360@gmail.com>
2023-10-24 14:36:48 +0500

    DropDown test file practice

--- tests/DropDowns.spec.js
+++ tests/DropDowns.spec.js
@@ -17,44 +17,53 @@ test("Handle dropdowns",async ({page})=>{
   //await expect(options).toHaveCount(10);
 
   //2) check number of options in dropdown - Appraoch2
-  //const options=await page.$$('#country option')
+  // const options=await page.$$('#country option')
   //console.log("Number of options:", options.length)
  // await expect(options.length).toBe(10);
 
 //3) check presence of value in the dropdown - Appraoch1
 // const content=await page.locator('#country').textContent()
- //await expect(content.includes('India')).toBeTruthy();
+//  await expect(content.includes('India')).toBeTruthy();
 
 //4) check presence of value in the dropdown - Approach 2 - using looping
-/*const options=await page.$$('#country option')
-let status=false;
-for(const option of options)
-{
-  //console.log(await option.textContent())
-  let value=await option.textContent();
-  if(value.includes('France'))
-  {
-    status=true;
-    break;
-  }
-}
-expect(status).toBeTruthy();
-*/
+// const options=await page.$$('#country option')
+// let status=false;
+// for(const option of options)
+// {
+//   console.log(await option.textContent())
+//   let value=await option.textContent();
+//   if(value.includes('France'))
+//   {
+//     status=true;
+//     break;
+//   }
+// }
+// expect(status).toBeTruthy();
 
 
-//5) select option from dropdown using loop 
+
+// //5) select option from dropdown using loop 
+// const options=await page.$$('#country option')
+// for(const option of options)
+// {
+//   let value=await option.textContent();
+//   if(value.includes('France'))
+//   {
+//     await page.selectOption("#country", value);
+//     break;
+//   }
+// }
+
+
+
+//6) select all options 1 by 1 from dropdown using loop 
 const options=await page.$$('#country option')
 for(const option of options)
 {
   let value=await option.textContent();
-  if(value.includes('France'))
-  {
-    await page.selectOption("#country", value);
-    break;
-  }
+  console.log(value);
+  await page.selectOption("#country", value);
 }
-
-
     await page.waitForTimeout(5000);
 
 })
